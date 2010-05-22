e2dfa7fe4b85e525829c8aabae6dafbc3f57fa76
Tom Helmuth <trhtom@gmail.com>
2010-05-21 20:41:44 -0400

    Cart centering problem added to problem classes.

--- PshGP.java
+++ PshGP.java
@@ -1,31 +1,31 @@
-
 //import java.util.*;
 import java.io.*;
 
 import org.spiderland.Psh.*;
 
 public class PshGP {
-    public static void main( String args[] ) throws Exception {
-	if( args.length != 1 && args.length != 3 ) {
-	    System.out.println( "Usage: PshGP paramfile|checkpointfile.gz [testprogram testcasenumber]" );
-	    System.exit( 0 );
+	public static void main(String args[]) throws Exception {
+		if (args.length != 1 && args.length != 3) {
+			System.out
+					.println("Usage: PshGP paramfile|checkpointfile.gz [testprogram testcasenumber]");
+			System.exit(0);
+		}
+
+		GA ga = null;
+		if (args[0].endsWith(".gz"))
+			ga = GA.GAWithCheckpoint(args[0]);
+		else
+			ga = GA.GAWithParameters(Params.ReadFromFile(new File(args[0])));
+
+		if (args.length == 3) {
+			// Execute a test program
+
+			Program p = new Program(args[1]);
+			((PushGP) ga).RunTestProgram(p, Integer.parseInt(args[2]));
+		} else {
+			// Execute a GP run.
+
+			ga.Run();
+		}
 	}
-
-        GA ga = null;
-        if (args[0].endsWith(".gz"))
-            ga = GA.GAWithCheckpoint(args[0]);
-        else
-            ga = GA.GAWithParameters( Params.ReadFromFile( new File( args[ 0 ] ) ) );
-
-	if( args.length == 3 ) {
-	    // Execute a test program
-
-	    Program p = new Program( args[ 1 ] );
-	    ((PushGP)ga).RunTestProgram( p, Integer.parseInt( args[ 2 ] ) );
-	} else {
-	    // Execute a GP run.
-
-	    ga.Run();
-	}
-    }
 }
